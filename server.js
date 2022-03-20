2d172a36111911c2f9fedac1949c44c8b6979e1c
Robin Huo <robin.huo@mail.utoronto.ca>
2022-03-20 14:21:57 -0400

    Create server

---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/pub")));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Express listening on port ${port}...`);
+});
