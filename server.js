a266b2d544882fed3870a405a136be748f84aae4
Robin Huo <robin.huo@mail.utoronto.ca>
2022-03-20 18:18:12 -0400

    Redirect from / to /examples.html

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "/pub")));
 
+app.get("/", (req, res) => {
+    res.redirect("/examples.html");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Express listening on port ${port}...`);
