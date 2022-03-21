7cd6fa64859f5ee4ef2dbb9b52ec1b61188fc079
Robin Huo <robin.huo@mail.utoronto.ca>
2022-03-20 21:39:53 -0400

    Documentation

--- pub/phonolo.js
+++ pub/phonolo.js
@@ -1,6 +1,15 @@
 function Phonolo() {
     "use strict";
 
+    /**
+     * Add an event listener to element that will create a popup.
+     * The event type is given by event.
+     * The popup element should be created and returned by getPopup.
+     * 
+     * @param {HTMLElement} element 
+     * @param {string} event 
+     * @param {function} getPopup 
+     */
     function addPopup(element, event, getPopup) {
         element.addEventListener(event, e => {
             const popup = getPopup();
@@ -16,18 +25,64 @@ function Phonolo() {
         });
     }
 
+    /**
+     * Clear all popups in the document.
+     * 
+     * @param {Event} [e]
+     */
     function clearPopups(e) {
         if (!e || !e.target.matches(".phonolo-popup, .phonolo-popup *"))
             document.querySelectorAll(".phonolo-popup").forEach(popup => { popup.remove(); });
     }
 
 
+    /**
+     * A class representing a feature system or a segment inventory.
+     */
     class Inventory {
 
+        /**
+         * A dictionary of the segments in the inventory.
+         * Maps segment symbol to Phone objects.
+         * 
+         * @type {Object.<string, Phone>}
+         */
         segments = {};
+
+        /**
+         * A dictionary mapping features to feature values and segments.
+         * Keys are feature names.
+         * Values are objects whose keys are feature values
+         * and values are arrays containing the Phone objects
+         * in this.segments that have the corresponding value
+         * for the corresponding feature.
+         * 
+         * @type {Object.<string, Object.<string, Array.<Phone>>>}
+         */
         features = {};
+
+        /**
+         * A reference to another Inventory instance that models
+         * the feature system used by this inventory.
+         * That is, the segments and features in this inventory
+         * should be a subset of those in featureSystem.
+         * Optional.
+         * 
+         * @type {?Inventory}
+         */
         featureSystem;
 
+        /**
+         * Return an Inventory object parsed from obj.
+         * obj should be an object whose keys are Phone symbols
+         * and values are feature specifications
+         * (objects whose keys are features names and values are feature values).
+         * If json is true then obj is assumed to be a JSON string.
+         * 
+         * @param {(Object.<string, Object.<string, string>>|string)} obj 
+         * @param {boolean} json 
+         * @returns {Inventory}
+         */
         static fromObject(obj, json = false) {
             if (json) obj = JSON.parse(obj);
             const segments = [];
@@ -37,6 +92,14 @@ function Phonolo() {
             return new Inventory(segments);
         }
 
+        /**
+         * Return an Inventory object based off featureSystem that contains
+         * all the given segments, which is taken in as an array of segment symbols.
+         * 
+         * @param {Inventory} featureSystem
+         * @param {string} segments
+         * @returns {Inventory}
+         */
         static fromFeatureSystem(featureSystem, segments) {
             const phonemes = [];
             for (const segment of segments) {
@@ -45,6 +108,13 @@ function Phonolo() {
             return new Inventory(phonemes, featureSystem);
         }
 
+        /**
+         * Create a new Inventory with the given segments
+         * and optionally based off the given feature system.
+         * 
+         * @param {Array.<Phone>} segments
+         * @param {Inventory} [featureSystem]
+         */
         constructor(segments, featureSystem) {
             for (const segment of segments) {
                 this.addSegment(segment);
@@ -70,9 +140,20 @@ function Phonolo() {
             );
         }
 
+        /**
+         * Parse the given string as a transcription using the segments in this inventory
+         * and return an array of these segments.
+         * 
+         * @throws Throws an error if the text cannot be fully parsed.
+         * @param {string} text
+         * @returns {Array.<Phone>}
+         */
         parse(text) {
+            // Replacement pattern to escape all special regex symbols
             // https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
             const escape = /[-\/\\^$*+?.()|[\]{}]/g;
+
+            // Union of all the symbols in this.segments
             const segmentRegex = new RegExp(`(${
                 Object.keys(this.segments)
                     .sort()
@@ -98,23 +179,62 @@ function Phonolo() {
     }
 
 
+    /**
+     * A class representing a segment.
+     */
     class Phone {
 
+        // Constants for use in rules
         static NULL = new Phone("∅");
         static C = new Phone("C");
         static V = new Phone("V");
         static WORD_BOUNDARY = new Phone("#");
 
+        /**
+         * The symbol used to transcribe this segment.
+         * 
+         * @type {string}
+         */
         symbol;
+
+        /**
+         * The feature specification for this segment.
+         * Keys are feature names and values are feature values.
+         * 
+         * @type {Object.<string, string>}
+         */
         features = {};
+
+        /**
+         * The name of this segment.
+         * Optional.
+         * 
+         * @type {?string}
+         */
         name;
 
+        /**
+         * Create a new Phone with the given symbol, features, and name.
+         * 
+         * @param {string} symbol
+         * @param {Object.<string, string>} features
+         * @param {string} [name]
+         */
         constructor(symbol, features, name) {
             this.symbol = symbol.normalize();
             if (features) this.features = features;
             if (name) this.name = name;
         }
 
+        /**
+         * Create and return a DOM element representing this segment.
+         * If withPopup is true and an inventory is provided,
+         * the created DOM element will be interactive.
+         * 
+         * @param {boolean} [withPopup=false]
+         * @param {Inventory} [inventory]
+         * @returns {HTMLElement}
+         */
         createElement(withPopup = false, inventory) {
             const elem = document.createElement("span");
             elem.classList.add("phonolo", "phonolo-phone");
@@ -155,13 +275,47 @@ function Phonolo() {
     }
 
 
+    /**
+     * A class representing a word with a phonetic/phonemic transcription.
+     */
     class Word {
 
+        /**
+         * The original text of the word.
+         * 
+         * @type {string}
+         */
         text;
+
+        /**
+         * Transcription of the word.
+         * 
+         * @type {Array.<(Phone|string)>}
+         */
         transcription;
+
+        /**
+         * The inventory that the segments in the transcription belong to.
+         * Optional.
+         * 
+         * @type {?Inventory}
+         */
         inventory;
+
+        /**
+         * The DOM element for this Word.
+         * 
+         * @type {HTMLElement}
+         */
         element;
 
+        /**
+         * Create a new Word with the given text and transcription.
+         * 
+         * @param {string} text
+         * @param {Array.<(Phone|string)>} transcription
+         * @param {Inventory} [inventory]
+         */
         constructor(text, transcription, inventory) {
             this.text = text;
             this.transcription = transcription;
@@ -178,6 +332,11 @@ function Phonolo() {
             this.showText();
         }
 
+        /**
+         * Get the DOM element for this Word.
+         * 
+         * @returns {HTMLElement} the DOM element for this Word
+         */
         getElement() {
             if (!this.element) {
                 this.createElement();
@@ -199,14 +358,38 @@ function Phonolo() {
     }
 
 
+    /**
+     * A class representing a feature bundle.
+     */
     class FeatureBundle {
 
+        /**
+         * Feature specification for this feature bundle.
+         * 
+         * @type {Object.<string, string>}
+         */
         features;
 
+        /**
+         * Create a new FeatureBundle with the given features.
+         * 
+         * @param {Object.<string, string>} features 
+         */
         constructor(features) {
             this.features = features;
         }
 
+        /**
+         * Create and return a DOM element representing this feature bundle.
+         * If withPopup is true and inventory is given, the created element
+         * will be interactive.
+         * If brackets is true (default), the bundle will be drawn with brackets.
+         * 
+         * @param {boolean} [withPopup=false]
+         * @param {Inventory} [inventory]
+         * @param {boolean} [brackets=true]
+         * @returns 
+         */
         createElement(withPopup = false, inventory, brackets = true) {
             const elem = document.createElement("div");
             elem.classList.add("phonolo", "phonolo-featurebundle");
@@ -294,13 +477,47 @@ function Phonolo() {
     }
 
 
+    /**
+     * A class representing a linear phonological rule.
+     */
     class Rule {
 
+        /**
+         * The target of the rule.
+         * 
+         * @type {(Phone|FeatureBundle)}
+         */
         target;
+
+        /**
+         * The result of the rule.
+         * 
+         * @type {(Phone|FeatureBundle)}
+         */
         result;
+
+        /**
+         * The left environment of the rule.
+         * 
+         * @type {Array.<(Phone|FeatureBundle)>}
+         */
         environmentLeft;
+
+        /**
+         * The right environment of the rule.
+         * 
+         * @type {Array.<(Phone|FeatureBundle)>}
+         */
         environmentRight;
 
+        /**
+         * Create a new Rule with the given parameters.
+         * 
+         * @param {(Phone|FeatureBundle)} target
+         * @param {(Phone|FeatureBundle)} result
+         * @param {Array.<(Phone|FeatureBundle)>} environmentLeft
+         * @param {Array.<(Phone|FeatureBundle)>} environmentRight
+         */
         constructor(target, result, environmentLeft, environmentRight) {
             this.target = target;
             this.result = result;
@@ -308,6 +525,13 @@ function Phonolo() {
             this.environmentRight = environmentRight;
         }
 
+        /**
+         * Create and return a new DOM element representing this rule.
+         * If inventory is given, the created element will be interactive.
+         * 
+         * @param {Inventory} [inventory]
+         * @returns 
+         */
         createElement(inventory) {
             const elem = document.createElement("div");
             elem.classList.add("phonolo", "phonolo-rule");
